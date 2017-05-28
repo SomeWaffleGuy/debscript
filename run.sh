04eb23fbb0f264b6b0a1805015abd7a0532524ee
SomeWaffleGuy <kristofergreig@gmail.com>
2017-05-28 17:59:42 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,8 +7,6 @@ then
 	echo "$(tput setaf 1)$(tput bold)Must run as non-root user$(tput sgr 0)"
 	exit
 fi
-touch show-clock-date
-touch 12-hour
 echo -n "$(tput setaf 2)$(tput bold)Which Debian version do you wish to run?
 1: Testing
 2: Sid
@@ -17,32 +15,12 @@ read answer
 if echo "$answer" | grep -iq "^1" ;then
 	wget https://raw.githubusercontent.com/SomeWaffleGuy/debscript/master/testing.sh
 	chmod +x testing.sh
-	su -c './testing.sh'
+	sudo './testing.sh'
 elif echo "$answer" | grep -iq "^2" ;then
 	wget https://raw.githubusercontent.com/SomeWaffleGuy/debscript/master/sid.sh
 	chmod +x sid.sh
-	su -c './sid.sh'
+	sudo './sid.sh'
 fi
-gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
-gsettings set org.gnome.desktop.interface show-battery-percentage 'true'
 gsettings set org.gnome.settings-daemon.plugins.xsettings antialiasing 'rgba'
-gsettings set org.gnome.desktop.interface gtk-theme "Arc-Darker"
-gsettings set org.gnome.desktop.interface icon-theme "Moka"
-gsettings set org.gnome.desktop.interface cursor-theme "DMZ-White"
-gsettings set org.gnome.shell.extensions.user-theme name "Arc-Dark"
-gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Cantarell Bold 10'
-gsettings set org.gnome.desktop.interface font-name 'Cantarell 10'
-gsettings set org.gnome.desktop.interface document-font-name 'Sans 10'
-gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace 10'
-if [ $(cat show-clock-date) = true ]
-then
-gsettings set org.gnome.desktop.interface clock-show-date "true"
-fi
-if [ $(cat 12-hour) = true ]
-then
-gsettings set org.gnome.desktop.interface clock-format "12h"
-fi
-rm show-clock-date 12-hour user
-echo "$(tput setaf 1)$(tput bold)Enable 'User themes' in Tweak Tool for matching shell theme$(tput sgr 0)"
 echo "$(tput setaf 1)$(tput bold)Some features won't work until after a reboot$(tput sgr 0)"
 exit 0
