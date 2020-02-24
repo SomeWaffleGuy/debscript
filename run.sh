132efee0489c70ac7fc625e4e6fd776a176b417e
SomeWaffleGuy <kristofergreig@gmail.com>
2020-02-24 16:11:00 -0500

    Added Stable and noted Testing as not recomended

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-#Scrip with the intention of setting up a custom Debian Testing or Sid install from current Testing iso, may or may not ever actually work
+#Scrip with the intention of setting up a custom Debian install from current Stable or Testing iso, may or may not ever actually work
 echo "$(tput setaf 1)$(tput bold)An Internet connection is REQUIRED$(tput sgr 0)"
 echo "$USER" > user
 if [ $(cat user) = root ]
@@ -8,15 +8,20 @@ then
 	exit
 fi
 echo -n "$(tput setaf 2)$(tput bold)Which Debian version do you wish to run?
-1: Testing
-2: Sid
+1: Stable
+2: Testing (NOT Reccomended)
+3: Sid
 $(tput sgr 0)"
 read answer
 if echo "$answer" | grep -iq "^1" ;then
-	wget https://raw.githubusercontent.com/SomeWaffleGuy/debscript/master/testing.sh
+	wget https://raw.githubusercontent.com/SomeWaffleGuy/debscript/master/stable.sh
 	chmod +x testing.sh
 	sudo './testing.sh'
 elif echo "$answer" | grep -iq "^2" ;then
+	wget https://raw.githubusercontent.com/SomeWaffleGuy/debscript/master/testing.sh
+	chmod +x testing.sh
+	sudo './testing.sh'
+elif echo "$answer" | grep -iq "^3" ;then
 	wget https://raw.githubusercontent.com/SomeWaffleGuy/debscript/master/sid.sh
 	chmod +x sid.sh
 	sudo './sid.sh'
