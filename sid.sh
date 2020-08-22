ca3d03cd20b72b0df9491b0c4873f4d2371cb0cc
SomeWaffleGuy <kristofergreig@gmail.com>
2020-08-22 13:21:16 -0400

    Update sid.sh
    Added better 'typical' install. sources.list now comments out source packages. Flathub is added for easy of installing and some applications are offered/used in the typical install. GNOME Weathers is kept for weather functionality (Previously I'd suggest OpenWeather, but on newer versions of GNOME the GNOME Weather application works just as well if not better).

--- sid.sh
+++ sid.sh
@@ -2,30 +2,24 @@
 #Sid installer/customizer
 echo "$(tput setaf 2)$(tput bold)Uninstalling useless GNOME parts$(tput sgr 0)"
 sleep 3
-	apt purge -y gnome-maps gnome-music gnome-photos gnome-games gnome-documents gnome-weather gnome-dictionary polari shotwell xterm
+	apt purge -y gnome-maps gnome-music gnome-photos gnome-games gnome-documents gnome-dictionary polari shotwell xterm libreoffice*
 	apt autoremove --purge -y
 echo "$(tput setaf 2)$(tput bold)Enabling HTTPS for APT$(tput sgr 0)"
 sleep 3
 	apt install -y apt-transport-https
 echo "$(tput setaf 2)$(tput bold)Modifying sources.list and upgrading$(tput sgr 0)"
 sleep 3
-	echo "# Debian Sid
-deb https://deb.debian.org/debian/ sid main non-free contrib
-deb-src https://deb.debian.org/debian/ sid main non-free contrib" > /etc/apt/sources.list
+	echo "# Debian Stable
+deb https://deb.debian.org/debian/ sid main contrib non-free
+#deb-src https://deb.debian.org/debian/ sid main contrib non-free" > /etc/apt/sources.list
 	apt update
 	apt dist-upgrade -y
 sleep 3
-echo -n "$(tput setaf 2)$(tput bold)Show date on clock (does not work on GDM)?$(tput sgr 0) "
-read answer
-if echo "$answer" | grep -iq "^y" ;then
-	echo "true" > show-clock-date
-fi
-echo -n "$(tput setaf 2)$(tput bold)Use 12 hour time?$(tput sgr 0) "
+echo -n "$(tput setaf 2)$(tput bold)Use 12 hour time on GDM?$(tput sgr 0) "
 read answer
 if echo "$answer" | grep -iq "^y" ;then
 	echo "[org/gnome/desktop/interface]
 clock-format='12h'" >> /etc/gdm3/greeter.dconf-defaults
-	echo "true" > 12-hour
 fi
 dpkg-reconfigure gdm3
 echo -n "$(tput setaf 2)$(tput bold)Select Install options
@@ -62,7 +56,10 @@ if echo "$answer" | grep -iq "^2" ;then
 	echo -n "$(tput setaf 2)$(tput bold)Install proprietary Nvidia drivers?$(tput sgr 0) "
 	read answer
 	if echo "$answer" | grep -iq "^y" ;then
-		apt install -y nvidia-driver
+		apt install -y nvidia-driver nvidia-cuda-dev nvidia-cuda-toolkit
+		apt install -y nvidia-driver-libs:i386
+		echo "options nvidia-drm modeset=1" > /etc/modprobe.d/nvidia-drm-nomodeset.conf
+		sudo update-initramfs -u
 	fi
 	echo -n "$(tput setaf 2)$(tput bold)Enable KMS (Proprietary Nvidia users can skip this)?$(tput sgr 0) "
 	read answer
@@ -141,27 +138,30 @@ $(tput sgr 0)"
 		echo -n "$(tput setaf 2)$(tput bold)Install Steam?$(tput sgr 0) "
 		read answer
 		if echo "$answer" | grep -iq "^y" ;then
-			apt install -y steam
+			apt install -y steam dxvk
 		fi
 	fi
 	echo -n "$(tput setaf 2)$(tput bold)Install Flatpak support?$(tput sgr 0) "
 	read answer
 	if echo "$answer" | grep -iq "^y" ;then
 		apt install -y flatpak gnome-software-plugin-flatpak
+		flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	fi
+	fi
+	echo -n "$(tput setaf 2)$(tput bold)Install typical Flatpak applications?$(tput sgr 0) "
+	if echo "$answer" | grep -iq "^y" ;then
+		flatpak install org.gtk.Gtk3theme.Adwaita-dark org.libreoffice.LibreOffice org.gimp.GIMP org.signal.Signal com.discordapp.Discord
 	fi
 elif echo "$answer" | grep -iq "^1" ;then
 	#Install all of the usual things
 	dpkg --add-architecture i386
 	apt update
-	apt install -y firmware-linux-nonfree libavcodec-extra mpv youtube-dl ttf-mscorefonts-installer fonts-cabin fonts-comfortaa fonts-croscore fonts-ebgaramond fonts-ebgaramond-extra fonts-font-awesome fonts-freefont-otf fonts-freefont-ttf fonts-gfs-artemisia fonts-gfs-complutum fonts-gfs-didot fonts-gfs-neohellenic fonts-gfs-olga fonts-gfs-solomos fonts-junicode fonts-lmodern fonts-lobster fonts-lobstertwo fonts-noto-hinted fonts-oflb-asana-math fonts-sil-gentiumplus fonts-sil-gentiumplus-compact fonts-stix fonts-texgyre fonts-arphic-ukai fonts-arphic-uming fonts-ipafont-mincho fonts-ipafont-gothic fonts-unfonts-core fonts-roboto fonts-symbola unrar zip plymouth plymouth-themes chromium pepperflashplugin-nonfree chromium-widevine torbrowser-launcher apparmor apparmor-profiles apparmor-profiles-extra apparmor-utils steam
+	apt install -y linux-headers-amd64 firmware-linux-nonfree libavcodec-extra mpv youtube-dl ttf-mscorefonts-installer fonts-cabin fonts-comfortaa fonts-croscore fonts-ebgaramond fonts-ebgaramond-extra fonts-font-awesome fonts-freefont-otf fonts-freefont-ttf fonts-gfs-artemisia fonts-gfs-complutum fonts-gfs-didot fonts-gfs-neohellenic fonts-gfs-olga fonts-gfs-solomos fonts-junicode fonts-lmodern fonts-lobster fonts-lobstertwo fonts-noto-hinted fonts-oflb-asana-math fonts-sil-gentiumplus fonts-sil-gentiumplus-compact fonts-stix fonts-texgyre fonts-arphic-ukai fonts-arphic-uming fonts-ipafont-mincho fonts-ipafont-gothic fonts-unfonts-core fonts-roboto fonts-symbola unrar zip plymouth plymouth-themes apparmor apparmor-profiles apparmor-profiles-extra apparmor-utils steam dxvk flatpak gnome-software-plugin-flatpak firefox
 	apt purge firefox-esr
-	apt autoremove --purge -y
-	#Setup Widevine
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-	dpkg -x google-chrome-stable_current_amd64.deb ~/tmp
-	mv ~/tmp/opt/google/chrome/libwidevinecdm* /usr/lib/chromium/
-	rm -rf ~/tmp
-	rm google-chrome-stable_current_amd64.deb
+	apt autoremove --purge
+	#Add Flathub and install Flatpak applications
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	flatpak install org.gtk.Gtk3theme.Adwaita-dark org.libreoffice.LibreOffice org.gimp.GIMP org.signal.Signal com.discordapp.Discord
 	#Ask for GPU to set up KMS
 	echo -n "$(tput setaf 2)$(tput bold)Which GPU do you have?
 1: Intel
@@ -184,7 +184,10 @@ radeon modeset=1" >> /etc/initramfs-tools/modules
 drm
 nouveau modeset=1" >> /etc/initramfs-tools/modules
 	elif echo "$answer" | grep -iq "^4" ;then
-		apt install -y nvidia-driver
+		apt install -y nvidia-driver nvidia-cuda-dev nvidia-cuda-toolkit
+		apt install -y nvidia-driver-libs:i386
+		echo "options nvidia-drm modeset=1" > /etc/modprobe.d/nvidia-drm-nomodeset.conf
+		sudo update-initramfs -u
 	fi
 	#Edit GRUB as needed, set Plymouth theme
 	perl -pi -e 's,GRUB_CMDLINE_LINUX_DEFAULT="(.*)"$,GRUB_CMDLINE_LINUX_DEFAULT="$1 splash",' /etc/default/grub
